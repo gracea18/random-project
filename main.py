3f3137b544e0172b97d17b544d1af1c3c51fe708
gracea18 <72771341+gracea18@users.noreply.github.com>
2021-02-07 03:44:33 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,7 @@
+
+while True:
+    if input.rotation(Rotation.PITCH) > 0 or input.rotation(Rotation.PITCH) < 0 or input.rotation(Rotation.ROLL) > 0 or input.rotation(Rotation.ROLL) < 0:
+        light.set_all(light.rgb(255,255,255)
+        music.set_volume(100)
+        music.siren.play_until_done()
+
